9087c311fb0e07107484396c73d68fe235c0c9ff
gerald <michaelgeraldarcega@gmail.com>
2013-11-07 14:06:26 +0800

    static dashboard

--- application/views/templates/user_template.php
+++ application/views/templates/user_template.php
@@ -177,10 +177,7 @@
                 type: 'column'
             },
             title: {
-                text: 'Monthly Average Rainfall'
-            },
-            subtitle: {
-                text: 'Source: WorldClimate.com'
+                text: 'Monthly Score Average'
             },
             xAxis: {
                 categories: [
@@ -201,7 +198,7 @@
             yAxis: {
                 min: 0,
                 title: {
-                    text: 'Rainfall (mm)'
+                    text: 'Scores'
                 }
             },
             tooltip: {
@@ -222,20 +219,8 @@
 	            enabled: false
 	        },
             series: [{
-                name: 'Tokyo',
-                data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-    
-            }, {
-                name: 'New York',
-                data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-    
-            }, {
-                name: 'London',
-                data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-    
-            }, {
-                name: 'Berlin',
-                data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+                name: '2013',
+                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3.5, 0]
     
             }]
         });
@@ -279,10 +264,10 @@
                 type: 'bar'
             },
             title: {
-                text: 'Stacked bar chart'
+                text: 'Goals Status'
             },
             xAxis: {
-                categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+                categories: ['Improve Patient Care', 'Maintain Good Work Environment', 'Ensure Optimal Quality of Care', 'Improve Nursing Research' ]
             },
             yAxis: {
                 min: 0,
@@ -303,14 +288,20 @@
                 }
             },
                 series: [{
-                name: 'John',
-                data: [5, 3, 4, 7, 2]
+                name: 'At Risk',
+                data: [5, 3, 4, 7]
             }, {
-                name: 'Jane',
-                data: [2, 2, 3, 2, 1]
+                name: 'Warning',
+                data: [2, 2, 3, 2]
             }, {
-                name: 'Joe',
-                data: [3, 4, 4, 2, 5]
+                name: 'Completed',
+                data: [3, 4, 4, 2]
+            }, {
+                name: 'On-Going',
+                data: [3, 4, 4, 2]
+            }, {
+                name: 'Pending',
+                data: [3, 4, 4, 2]
             }]
         });
 
