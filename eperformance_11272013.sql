2e49f8770a0889de40927930aa9941a60362cd8d
gerald <michaelgeraldarcega@gmail.com>
2013-11-28 07:42:23 +0800

    add sql file

---
+++ eperformance_11272013.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.5.34-0ubuntu0.12.04.1 : Database - eperformance
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `tbl_emp_development` */
+
+DROP TABLE IF EXISTS `tbl_emp_development`;
+
+CREATE TABLE `tbl_emp_development` (
+  `user_id` int(11) DEFAULT NULL,
+  `training_id` int(11) DEFAULT NULL,
+  `date_start` date DEFAULT NULL,
+  `date_end` date DEFAULT NULL,
+  `comment` text NOT NULL,
+  `status` enum('Pending','On-going','Completed','Cancelled') NOT NULL,
+  `date_assigned` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `user_id` (`user_id`),
+  KEY `training_id` (`training_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
