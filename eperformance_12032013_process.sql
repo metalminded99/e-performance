7a6cc8d89c232fbde6267340b375720935f48f1c
gerald <michaelgeraldarcega@gmail.com>
2013-12-03 10:42:53 +0800

    added sql file

---
+++ eperformance_12032013_process.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.5.34-0ubuntu0.12.04.1 : Database - eperformance
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `eperformance`;
+
+/*Table structure for table `tbl_process` */
+
+DROP TABLE IF EXISTS `tbl_process`;
+
+CREATE TABLE `tbl_process` (
+  `proc_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proc_title` varchar(100) DEFAULT NULL,
+  `proc_desc` text,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `reminder` int(11) NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`proc_id`),
+  KEY `proc_title` (`proc_title`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
